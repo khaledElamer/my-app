e73985921742b4e1d84a519c4b9703fa7692e52f
khaled mohamed <khaled.mohamed@segmatek.com>
2023-05-25 13:12:18 +0300

    commit

--- src/Component/AddClinic.jsx
+++ src/Component/AddClinic.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
 import { useHistory } from 'react-router-dom';
 import ClinicComponent from './ClinicComponent'; // Import the ClinicComponent
 import './AddClinic.css'; // Import the CSS file
 
 const countries = [
   { name: 'Country 1', code: 'C1' },
-  { name: 'Country 2', code: 'C2' },
+  { name: 'Country 2', code: '2' },
   // Add more countries as needed
 ];
 
 const governorates = [
-  { name: 'Governorate 1', code: 'G1' },
+  {name: 'Governorate 1', code: 'G1' },
   { name: 'Governorate 2', code: 'G2' },
   // Add more governorates as needed
 ];
@@ -29,20 +30,31 @@ const clinics = [
   // Add more clinics as needed
 ];
 
-const AddClinic = () => {
+ const AddClinic = () => {
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [selectedGovernorate, setSelectedGovernorate] = useState(null);
   const [selectedSpecialist, setSelectedSpecialist] = useState(null);
   const [selectedClinic, setSelectedClinic] = useState(null);
+  const [hasInsurance, setHasInsurance] = useState(false); // State to control checkbox for medical insurance
   const [showClinicComponent, setShowClinicComponent] = useState(false); // State to control rendering of ClinicComponent
+  const [loading, setLoading] = useState(false); // State to control loading state
   const history = useHistory();
 
   const handleSearch = () => {
-    // Perform search based on selectedCountry, selectedGovernorate, selectedSpecialist, and selectedClinic
-    // Redirect to the add clinic page or perform necessary action
-    history.push(`/clinic/${selectedCountry}/${selectedGovernorate}/${selectedSpecialist}/${selectedClinic}`);
+    if (!selectedCountry || !selectedGovernorate || !selectedSpecialist || !selectedClinic) {
+      alert('Please fill in all the fields before searching.');
+      return;
+    }
 
-    setShowClinicComponent(true); // Set showClinicComponent to true when search is clicked
+    setLoading(true); // Set loading state to true when search is clicked
+
+    // Perform search based on selectedCountry, selectedGovernorate, selectedSpecialist, selectedClinic, and hasInsurance
+    // Redirect to the clinic page or perform necessary action
+
+    setTimeout(() => {
+      setLoading(false); // Set loading state to false when search is complete
+      setShowClinicComponent(true); // Set showClinicComponent to true when search is clicked
+    }, 2000);
   };
 
   // Render ClinicComponent if showClinicComponent is true
@@ -94,11 +106,14 @@ const AddClinic = () => {
             onChange={(e) => setSelectedClinic(e.value)}
           />
         </div>
+        <div className="form-group">
+          <h3>Do you have medical insurance?</h3>
+          <Checkbox checked={hasInsurance} onChange={(e) => setHasInsurance(e.checked)} />
+        </div>
       </div>
-      <Button label="Search" onClick={handleSearch} className="search-button" />
+      <Button label={loading ? 'Loading...' : 'Search'} onClick={handleSearch} className="search-button" disabled={loading} />
     </div>
   );
 };
 
 export default AddClinic;
-    
--- src/Component/AddHospital.jsx
+++ src/Component/AddHospital.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
 import { useHistory } from 'react-router-dom';
+import { Checkbox } from 'primereact/checkbox';
 import './AddHospital.css'; // Import the CSS file
 import HospitalComponent from './HospitalComponent';
 
@@ -30,73 +31,67 @@ const hospitals = [
 ];
 
 const AddHospital = () => {
-const [selectedCountry, setSelectedCountry] = useState(null);
-const [selectedGovernorate, setSelectedGovernorate] = useState(null);
-const [selectedSpecialist, setSelectedSpecialist] = useState(null);
-const [selectedHospital, setSelectedHospital] = useState(null);
-const [showHospitalComponent, setShowHospitalComponent] = useState(false); // State to control rendering of HospitalComponent
-const history = useHistory();
+    const [selectedCountry, setSelectedCountry] = useState(null);
+    const [selectedGovernorate, setSelectedGovernorate] = useState(null);
+    const [selectedSpecialist, setSelectedSpecialist] = useState(null);
+    const [selectedHospital, setSelectedHospital] = useState(null);
+    const [showHospitalComponent, setShowHospitalComponent] = useState(false); // State to control rendering of HospitalComponent
+    const [loading, setLoading] = useState(false); // State to control loading state
+    const history = useHistory();
+    const [hasInsurance, setHasInsurance] = useState(false); // State to control checkbox for medical insurance
 
-const handleSearch = () => {
-// Perform search based on selectedCountry, selectedGovernorate, selectedSpecialist, and selectedHospital
-// Redirect to the add hospital page or perform necessary action
-
-setShowHospitalComponent(true); // Set showHospitalComponent to true when search is clicked
-};
-
-// Render HospitalComponent if showHospitalComponent is true
-if (showHospitalComponent) {
-return <HospitalComponent/>;
-}
-
-return (
-<div className="add-hospital-container">
-<h1>Select Country, Governorate, Specialist, and Hospital</h1>
-<div className="form-container">
-<div className="form-group">
-<h3>Country</h3>
-<Dropdown
-value={selectedCountry}
-options={countries}
-optionLabel="name"
-placeholder="Select a country"
-onChange={(e) => setSelectedCountry(e.value)}
-/>
-</div>
-<div className="form-group">
-<h3>Governorate</h3>
-<Dropdown
-value={selectedGovernorate}
-options={governorates}
-optionLabel="name"
-placeholder="Select a governorate"
-onChange={(e) => setSelectedGovernorate(e.value)}
-/>
-</div>
-<div className="form-group">
-<h3>Specialist</h3>
-<Dropdown
-value={selectedSpecialist}
-options={specialists}
-optionLabel="name"
-placeholder="Select a specialist"
-onChange={(e) => setSelectedSpecialist(e.value)}
-/>
-</div>
-<div className="form-group">
-<h3>Hospital</h3>
-<Dropdown
-value={selectedHospital}
-options={hospitals}
-optionLabel="name"
-placeholder="Select a hospital"
-onChange={(e) => setSelectedHospital(e.value)}
-/>
-</div>
-</div>
-<Button label="Search" onClick={handleSearch} className="search-button" />
-</div>
-);
-};
+    const handleSearch = () => {
+      if (!selectedCountry || !selectedGovernorate || !selectedSpecialist || !selectedHospital) {
+        alert('Please select all options before searching.');
+        return;
+      }
+  
+      setLoading(true); // Set loading state to true when search is clicked
+  
+      // Perform search based on selectedCountry, selectedGovernorate, selectedSpecialist, and selectedHospital
+      // Redirect to the add hospital page or perform necessary action
+  
+      setTimeout(() => {
+        setLoading(false); // Set loading state to false when search is complete
+        setShowHospitalComponent(true); // Set showHospitalComponent to true when search is clicked
+      }, 2000);
+    };
+  
+    // Render HospitalComponent if showHospitalComponent is true
+    if (showHospitalComponent) {
+      return <HospitalComponent />;
+    }
+  
+    return (
+      <div className="add-hospital-container">
+        <h1>Select Country, Governorate, Specialist, and Hospital</h1>
+        <div className="form-container">
+          <div className="form-group">
+            <h3>Country</h3>
+            <Dropdown value={selectedCountry} options={countries} optionLabel="name" placeholder="Select a country" onChange={(e) => setSelectedCountry(e.value)} />
+          </div>
+          <div className="form-group">
+            <h3>Governorate</h3>
+            <Dropdown value={selectedGovernorate} options={governorates} optionLabel="name" placeholder="Select a governorate" onChange={(e) => setSelectedGovernorate(e.value)} />
+          </div>
+          <div className="form-group">
+            <h3>Specialist</h3>
+            <Dropdown value={selectedSpecialist} options={specialists} optionLabel="name" placeholder="Select a specialist" onChange={(e) => setSelectedSpecialist(e.value)} />
+          </div>
+          <div className="form-group">
+            <h3>Hospital</h3>
+            <Dropdown value={selectedHospital} options={hospitals} optionLabel="name" placeholder="Select a hospital" onChange={(e) => setSelectedHospital(e.value)} />
+          </div>
+          <div className="form-group">
+          <h3>Do you have medical insurance?</h3>
+          <Checkbox checked={hasInsurance} onChange={(e) => setHasInsurance(e.checked)} />
+        </div>
+      
+        </div>
+        <Button label={loading ? 'Loading...' : 'Search'} onClick={handleSearch} className="search-button" disabled={loading} />
+      </div>
+    );
+  };
+  
 
 export default AddHospital;
